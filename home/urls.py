82b70db7ff298ae6f45437d78840a9920051f9f9
Pavan Adari <94852934+JustADude29@users.noreply.github.com>
2023-04-01 17:03:42 +0530

    login page kinda done

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('login_page', views.login_page, name='login_page'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "home/home_page.html")
+
+
+def login_page(request):
+    return render(request, "home/Authentication_Templates/login.html")
+
+
+# def login(request):
+#
